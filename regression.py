97cc70ef63e85c9d1c8563018cc6d4ba1432bb32
Joseph Ishola <joseph.arogunyo@gmail.com>
2025-03-07 14:24:43 +0100

    add a function to print minimum number in a list

--- regression.py
+++ regression.py
@@ -1,1 +1,12 @@
-print('Hello world')
+import numpy as np
+def Minimum(list):
+    min = np.inf
+    for i in range(len(list)):
+        if list[i] < min:
+            min = list[i]
+    print(min)
+
+
+Numbers = [5, 8, 51, 73, 37, 21, -8, 12, 6]
+
+Minimum(Numbers)
