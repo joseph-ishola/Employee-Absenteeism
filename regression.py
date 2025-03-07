a0d99b64960c28ef2aace5fef52d363f9c2d57c0
Joseph Ishola <joseph.arogunyo@gmail.com>
2025-03-07 14:09:29 +0100

    add a function to check maximum number in a list

--- regression.py
+++ regression.py
@@ -1,1 +1,11 @@
-print('Hello world')
+def Maximimum(list):
+    max = 0
+    for i in range(len(list)):
+        if list[i] > max:
+            max = list[i]
+    return max
+
+
+
+Numbers = [4, 52, 21, 95, 8, 25, 81]
+print(Maximimum(Numbers))
